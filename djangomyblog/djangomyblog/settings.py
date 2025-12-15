2f066f36e3eef371dde8623dd7605aa0125027cb
André Luferat <contato@luferat.net>
2025-12-15 12:36:22 -0300

    ok

--- djangomyblog/djangomyblog/settings.py
+++ djangomyblog/djangomyblog/settings.py
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Redirecionamentos de login
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = '/accounts/login/'
